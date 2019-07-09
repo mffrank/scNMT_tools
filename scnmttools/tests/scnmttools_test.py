699de76a94d59d87f29ff75716029395f4d0ebc3
Max Frank <max.florian.frank@gmail.com>
2019-07-09 18:57:11 +0200

    Create first test

--- scnmttools/tests/scnmttools_test.py
+++ scnmttools/tests/scnmttools_test.py
@@ -1,9 +1,17 @@
 import pytest
+import os
 
 from scnmttools import *
 
+print(os.getcwd())
+
+filenames = [
+    'E6.5_Plate3_G6.tsv.gz',
+    'E7.5_Plate1_A3.tsv.gz',
+    'E7.5_Plate3_F5.tsv.gz',
+    'E7.5_Plate4_B9.tsv.gz'
+]
+
 def test_data_avail():
-
-
-
-
+    for f in filenames:
+        assert os.path.exists(f), 'Example data %s not found'%f
