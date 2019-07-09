e79ed2ccf23f466118653e403d9c9288aec3b55e
Max Frank <max.florian.frank@gmail.com>
2019-07-09 17:40:51 +0200

    Setup minimal package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='scnmttools',
+      version='0.1',
+      description='Convenience functions and data handling stuff for scNMT in python',
+      url='https://github.com/mffrank/scNMT_tools',
+      author='Max Frank, Rene Snajder',
+      license='MIT',
+      packages=['scnmttools'],
+      zip_safe=False)
